729b1d43ecd15325d4e2e41ddc58c5ca92327f89
dohoonkim <kin061474@gmail.com>
2026-05-25 00:09:13 +0900

    caesar_code_2

--- caesar_code.py
+++ caesar_code.py
@@ -17,3 +17,13 @@ def solution(s, n):
             answer += char  # 공백인 경우 그대로 추가
             
     return answer
+
+# def caesar(s, n):
+#     s = list(s)
+#     for i in range(len(s)):
+#         if s[i].isupper():
+#             s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
+#         elif s[i].islower():
+#             s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
+
+#     return "".join(s)
